b84fd1611af95b1be622ab6d032bd29c010247f4
Rey <reybenedicto06@gmail.com>
2020-12-30 21:35:59 +0800

    Create slicer per book

---
+++ run.py
@@ -0,0 +1,23 @@
+import json
+
+
+# UTILS
+def write_to_file(filename, content):
+    f = open(filename, "a")
+    f.write(content)
+    f.close()
+#######
+
+
+
+# Read Whole Bible JSON then segregate per book
+def slice_per_book(whole_bible_json, output_folder):
+    with open(whole_bible_json) as json_file:
+        bible = json.load(json_file)
+        for book in bible.keys():
+            write_to_file(output_folder + "/" + book + ".txt", str(bible[book]))
+
+
+
+
+slice_per_book('source/AngDatingBiblia.json', 'output')
