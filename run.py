ce239c537b7b09e68fbc3f21db9028faff542cee
Rey <reybenedicto06@gmail.com>
2021-07-01 16:03:55 +0800

    Updated content

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import json
 
+BIBLE_JSON = "source/Ang Dating Biblia.json"
 
 # UTILS
 def write_to_file(filename, content):
@@ -15,9 +16,9 @@ def slice_per_book(whole_bible_json, output_folder):
     with open(whole_bible_json) as json_file:
         bible = json.load(json_file)
         for book in bible.keys():
-            write_to_file(output_folder + "/" + book + ".json", json.dumps(bible[book]))
+            write_to_file(output_folder + "/" + book + ".json", json.dumps(bible[book], separators=(',', ':')))
 
 
 
 
-slice_per_book('source/AngDatingBiblia.json', 'output')
+slice_per_book(BIBLE_JSON, 'output')
